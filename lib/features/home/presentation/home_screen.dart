30774ffa3d198302243af83fc957effde30e2bb7
Jesus Alberto Aguilar Martinez <jesus.aguilar@mithril.com.mx>
2025-10-01 17:17:09 -0600

    HomeScreen done

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -11,6 +11,23 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _body() {
-    return const Center(child: Text('Home Screen'));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _button(title: 'WebSocket', onPressed: () {}),
+          const SizedBox(height: 10),
+          _button(title: 'MQTT', onPressed: () {}),
+        ],
+      ),
+    );
+  }
+
+  Widget _button({required String title, required VoidCallback onPressed}) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.blue)),
+      child: Text(title, style: TextStyle(color: Colors.white)),
+    );
   }
 }
